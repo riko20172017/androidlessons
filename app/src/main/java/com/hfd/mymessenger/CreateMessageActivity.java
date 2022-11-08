3e2ca592589d6474fc80042dbaa8012524870c16
ganz <g_anz@mail.ru>
2022-11-08 22:24:43 +0300

    will add second activity

---
+++ app/src/main/java/com/hfd/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,19 @@
+package com.hfd.mymessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    protected void onSendMessage (View view) {
+
+    }
+}
